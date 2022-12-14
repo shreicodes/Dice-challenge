1e13d1e2efe0d0f5212a6efa300cb802d0174314
Sreelakshmi <118797813+shreicodes@users.noreply.github.com>
2022-12-14 14:32:03 +0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var randomNumber1= Math.random();
 randomNumber1=Math.floor(randomNumber1*6);
 randomNumber1=randomNumber1+1 ;
 
-var randomImage1= "/images/dice"+ randomNumber1+".png";
+var randomImage1= randomNumber1+".png";
 
 document.querySelector(".img1").setAttribute("src",randomImage1) ;
 
@@ -14,7 +14,7 @@ var randomNumber2= Math.random();
 randomNumber2=Math.floor(randomNumber2*6);
 randomNumber2=randomNumber2+1 ;
 
-var randomImage2= "/images/dice"+ randomNumber2+".png";
+var randomImage2= randomNumber2+".png";
 
 document.querySelector(".img2").setAttribute("src",randomImage2) ;
 
